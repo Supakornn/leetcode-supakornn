598fc16824b45567cb591365819ae0e5f684346a
Supakorn <supakorn0728@gmail.com>
2025-01-10 08:31:37 +0700

    :sparkles: add Longest Common Prefix

---
+++ 14. Longest Common Prefix/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		for j := 0; j < len(prefix); j++ {
+			if j >= len(strs[i]) || prefix[j] != strs[i][j] {
+				prefix = prefix[:j]
+				break
+			}
+		}
+	}
+
+	return prefix
+}
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"})) // Output: "fl"
+}
--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 - This repository is a collection of my personal LeetCode solutions.
 - I will try to solve as many problems as possible and keep updating this repository.
 
+- maybe I will add some explanations in the future.
+
 ### 📚 Table of Contents
 
 - [1. Two Sum](/1.%20Two%20Sum)
 - [9. Palindrome Number](/9.%20Palindrome%20Number)
 - [13. Roman to Integer](/13.%20Roman%20to%20Integer)
+- [14. Longest Common Prefix](/14.%20Longest%20Common%20Prefix)
