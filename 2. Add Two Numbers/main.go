6fd0a1b16ace5015740fbdd00024c0e25a1417e6
Supakornn <supakorn0728@gmail.com>
2025-03-25 01:02:04 +0700

    :fire: add two numbers

---
+++ 2. Add Two Numbers/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummyHead := &ListNode{}
+	current := dummyHead
+	carry := 0
+
+	for l1 != nil || l2 != nil {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		carry = sum / 10
+		current.Next = &ListNode{Val: sum % 10}
+		current = current.Next
+	}
+
+	if carry > 0 {
+		current.Next = &ListNode{Val: carry}
+	}
+
+	return dummyHead.Next
+}
+
+func main() {
+	fmt.Println(addTwoNumbers(&ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}},
+		&ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}})) // [7,0,8]
+}
